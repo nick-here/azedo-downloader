ae2b9f6da01c6b8dd632cc369fb2ebeb83d7d02e
nick-here <zuraniewski.karol@gmail.com>
2022-02-07 22:09:05 +0100

    navbar link click

---
+++ azedoDownloader.js
@@ -0,0 +1,12 @@
+(function () {
+	// copy and paste into browser's console
+
+	// Queries used to access poduct's download button
+	const nodeQueries = {
+		navbarLink: '#wide-nav .nav-top-link',
+		product: '.product-small a',
+		btn: '#somdn-form-submit-button',
+	};
+
+	document.querySelector(nodeQueries.navbarLink).click();
+})();
