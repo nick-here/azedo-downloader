cb0aef29457eb0b35e63c7760866884f52c08cc2
nick-here <zuraniewski.karol@gmail.com>
2022-02-10 21:59:58 +0100

    did stuff

--- azedoDownloader.js
+++ azedoDownloader.js
@@ -1,12 +1,55 @@
 (function () {
 	// copy and paste into browser's console
 
-	// Queries used to access poduct's download button
+	const DELAY = 100;
+	const STORAGE_COUNTER_KEY = 'currentProduct';
+
+	// queries used to access site's elements
 	const nodeQueries = {
 		navbarLink: '#wide-nav .nav-top-link',
 		product: '.product-small a',
-		btn: '#somdn-form-submit-button',
+		download: '#somdn-form-submit-button',
+		backToMain: '#error-page .wc-forward',
 	};
 
-	document.querySelector(nodeQueries.navbarLink).click();
+	const pathCallbacks = {
+		'': () => clickQuery(nodeQueries.navbarLink), // click navbar link
+		marka: () => clickQuery(nodeQueries.product), // click product
+		sklep: () => {
+			// click download
+			clickQuery(nodeQueries.download);
+
+			// increment product counter
+			localStorage.setItem(
+				STORAGE_COUNTER_KEY,
+				`${parseInt(localStorage.getItem(STORAGE_COUNTER_KEY)) + 1}`
+			);
+
+			moveBack();
+		}, // download and go back
+	};
+
+	// try clicking a query until its loaded
+	const clickQuery = queryName => {
+		const id = setInterval(() => {
+			const element = document.querySelector(queryName);
+			if (element) {
+				element.click();
+				console.log(`element of query "${queryName}" clicked`);
+				clearInterval(id);
+			}
+		}, 10);
+	};
+
+	const moveBack = () => {
+		console.log('moving back');
+		setTimeout(() => history.back(), DELAY);
+	};
+
+	const path = window.location.pathname.split('/')[1] ?? '';
+
+	// set default counter value to 0
+	if (!localStorage.getItem(STORAGE_COUNTER_KEY)) localStorage.setItem(STORAGE_COUNTER_KEY, '0');
+
+	pathCallbacks[path]();
 })();
