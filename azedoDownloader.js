91330e4e546eb9f663f74d72a6dde6c7766b2d47
nick-here <zuraniewski.karol@gmail.com>
2022-02-11 12:48:44 +0100

    update

--- azedoDownloader.js
+++ azedoDownloader.js
@@ -1,56 +1,26 @@
 (function () {
-	// copy and paste into browser's console
-
 	const DEBUG = true;
 
-	const DELAY = 100;
+	const DELAY = 1500;
 	const STORAGE_COUNTER_KEY = 'currentProduct';
+	const PRODUCTS_PER_PAGE = 12;
 
 	// queries used to access site's elements
 	const nodeQueries = {
-		navbarLink: '#wide-nav .nav-top-link',
-		product: '.product-small a',
-		download: '#somdn-form-submit-button',
-		backToMain: '#error-page .wc-forward',
+		navbarLink: i => `#wide-nav .nav-top-link:nth-of-type(${i + 1})`,
+		product: i => `.product-small:nth-of-type(${i + 1}) a`,
+		download: i => `#somdn-form-submit-button`,
+		backToMain: i => `#error-page .wc-forward`,
+		nextPage: i => '.next.page-number',
 	};
 
-	const pathCallbacks = {
-		'': () => {
-			/// click navbar link
-
-			clickQuery(nodeQueries.navbarLink);
-			DEBUG && console.log('entering tab');
-		},
-		marka: () => {
-			/// click product
-
-			clickQuery(nodeQueries.product);
-			DEBUG && console.log('entering product');
-		},
-		sklep: () => {
-			/// download and go back
-
-			// click download
-			clickQuery(nodeQueries.download);
-
-			// increment product counter
-			localStorage.setItem(
-				STORAGE_COUNTER_KEY,
-				`${parseInt(localStorage.getItem(STORAGE_COUNTER_KEY)) + 1}`
-			);
-
-			moveBack();
-
-			DEBUG && console.log('downloading');
-		},
-	};
-
-	// try clicking a query until its loaded
-	const clickQuery = queryName => {
+	// try clicking a query until it's loaded
+	const clickQuery = (queryName, callback = null) => {
 		const id = setInterval(() => {
 			const element = document.querySelector(queryName);
 			if (element) {
 				element.click();
+				if (callback) callback();
 				clearInterval(id);
 
 				DEBUG && console.log(`element of query "${queryName}" clicked`);
@@ -63,6 +33,58 @@
 			history.back();
 			DEBUG && console.log('moving back');
 		}, DELAY);
+	};
+
+	// aim at the next product, next page if 12 products opened
+	const nextProduct = () => {
+		const newVal =
+			(parseInt(localStorage.getItem(STORAGE_COUNTER_KEY)) + 1) % PRODUCTS_PER_PAGE;
+
+		// increment product counter
+		localStorage.setItem(STORAGE_COUNTER_KEY, `${newVal}`);
+
+		if (newVal === 0) nextPage();
+
+		DEBUG && console.log('next product');
+	};
+
+	const nextPage = () => {
+		const nextBtn = document.querySelector(nodeQueries.nextPage());
+
+		if (nextBtn) {
+			nextBtn.click();
+
+			DEBUG && console.log('next page');
+		} else if (DEBUG) {
+			console.log('last page met');
+			alert('last page met');
+		}
+	};
+
+	const pathCallbacks = {
+		'': () => {
+			/// click navbar link
+
+			clickQuery(nodeQueries.navbarLink(0));
+
+			DEBUG && console.log(`entering tab of index ${0}`);
+		},
+		marka: () => {
+			/// click product
+
+			const productIndex = parseInt(localStorage.getItem(STORAGE_COUNTER_KEY));
+			clickQuery(nodeQueries.product(productIndex));
+
+			DEBUG && console.log(`entering product of index ${productIndex}`);
+		},
+		sklep: () => {
+			/// download and go back
+
+			clickQuery(nodeQueries.download(), nextProduct);
+			moveBack();
+
+			DEBUG && console.log('downloading');
+		},
 	};
 
 	const path = window.location.pathname.split('/')[1] ?? '';
